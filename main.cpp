4ef007fb37ab9bdec051f29fb1d89d7c31f6ef4a
Ashroom66 <ashroom09@gmail.com>
2022-08-30 16:50:28 +0900

    feat: ボックスオブジェクト

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include "texture.h"
 #include "rtw_stb_image.h"
 #include "aarect.h"
+#include "box.h"
 
 #include <iostream>
 using namespace std;
@@ -129,6 +130,9 @@ hittable_list cornell_box () {
     objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
     objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
     objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
+
+    objects.add(make_shared<box>(point3(130, 0, 65), point3(295, 165, 230), white));
+    objects.add(make_shared<box>(point3(265, 0, 295), point3(430, 330, 460), white));
     return objects;
 }
 
